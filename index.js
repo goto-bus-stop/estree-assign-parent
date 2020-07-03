6e90ceb1c55ddd1477ad0c95630b2949a560bed2
Renée Kooi <renee@kooi.me>
2020-07-03 10:01:06 +0200

    use dash-ast

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var assert = require('assert')
+var dash = require('dash-ast')
 
 module.exports = function assignParent (ast, parentKey) {
   parentKey = parentKey || 'parent'
@@ -6,28 +7,11 @@ module.exports = function assignParent (ast, parentKey) {
   assert(isNode(ast), 'estree-assign-parent: ast must be a Node with a `type`')
   assert(typeof parentKey === 'string', 'estree-assign-parent: key must be a string')
 
-  walk(ast, null)
-  return ast
-
-  function walk (node, parent) {
-    var keys = Object.keys(node)
+  dash(ast, function (node, parent) {
     node[parentKey] = parent
-    for (var i = 0; i < keys.length; i++) {
-      var k = keys[i]
-      if (k === parentKey) continue
-      if (Array.isArray(node[k])) {
-        var elements = node[k]
-        for (var j = 0; j < elements.length; j++) {
-          if (isNode(elements[j])) {
-            walk(elements[j], node)
-          }
-        }
-      }
-      if (isNode(node[k])) {
-        walk(node[k], node)
-      }
-    }
-  }
+  })
+
+  return ast
 }
 
 function isNode (n) {
