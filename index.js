db6ee875d88dc00c9cfbfe07d6b461b32ccfe964
Renée Kooi <renee@kooi.me>
2020-07-03 10:19:44 +0200

    Revert "use dash-ast"
    This reverts commit 6e90ceb1c55ddd1477ad0c95630b2949a560bed2.

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var assert = require('assert')
-var dash = require('dash-ast')
 
 module.exports = function assignParent (ast, parentKey) {
   parentKey = parentKey || 'parent'
@@ -7,11 +6,28 @@ module.exports = function assignParent (ast, parentKey) {
   assert(isNode(ast), 'estree-assign-parent: ast must be a Node with a `type`')
   assert(typeof parentKey === 'string', 'estree-assign-parent: key must be a string')
 
-  dash(ast, function (node, parent) {
-    node[parentKey] = parent
-  })
-
+  walk(ast, null)
   return ast
+
+  function walk (node, parent) {
+    var keys = Object.keys(node)
+    node[parentKey] = parent
+    for (var i = 0; i < keys.length; i++) {
+      var k = keys[i]
+      if (k === parentKey) continue
+      if (Array.isArray(node[k])) {
+        var elements = node[k]
+        for (var j = 0; j < elements.length; j++) {
+          if (isNode(elements[j])) {
+            walk(elements[j], node)
+          }
+        }
+      }
+      if (isNode(node[k])) {
+        walk(node[k], node)
+      }
+    }
+  }
 }
 
 function isNode (n) {
